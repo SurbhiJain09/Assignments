67e4162e0ae453944746b36013a6c4af69e48fce
Surbhi Jain <surbhijain09@gmail.com>
2022-01-19 07:18:38 -0500

    Adding the .sh file

---
+++ LinuxAssignment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This is a script to create and delete directories"
+echo "Creating Dir1"
+mkdir Dir1
+echo "Creating Dir2"
+mkdir Dir2
+echo "Creating Dir3"
+mkdir Dir3
+echo "Deleting Dir2"
+rmdir Dir2
+
+
